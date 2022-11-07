bf36bff20b22159167524ac1eaba43a0d7ff6d76
Guillermo Nicrosi <erknardsieghartrage@gmail.com>
2022-11-07 20:40:02 -0300

    feat/bossDetailComponent

--- src/components/BossesList/BossesList.jsx
+++ src/components/BossesList/BossesList.jsx
@@ -1,25 +1,27 @@
 import { useEffect } from 'react';
 // Redux
-import { getAllBosses, getBossDetail } from '../../redux/slices/Bosses';
+import { getAllBosses } from '../../redux/slices/Bosses';
 import { useDispatch, useSelector } from 'react-redux';
+import {Link} from "react-router-dom";
 
 function BossesList() {
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(getAllBosses());
-		dispatch(getBossDetail('17f695abef9l0i1ujzrup5y0z7rw6'));
 	}, [dispatch]);
 
 	const { list: bosses } = useSelector(state => state.bosses);
 	return (
 		<div>
-			{bosses.map((boss, index) => (
+			{bosses.map((boss, index) => (				
 				<div key={index}>
+					<Link to={`/detail/${boss.id}`}>
 					<img src={boss.image} alt={boss.name} />
 					<h2>Name:{boss.name}</h2>
 					<h3>Region: {boss.region}</h3>
 					<p>Description: {boss.description}</p>
 					<p>Health: {boss.healthPoints}</p>
+					</Link>
 				</div>
 			))}
 		</div>
